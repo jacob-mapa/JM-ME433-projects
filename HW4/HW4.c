319e826e6360a721d9a7501993de2e71c195fbcc
jacob-mapa <jacobmapa2027@u.northwestern.edu>
2026-04-19 03:02:26 -0500

    set up initial code for HW4

--- HW4/HW4.c
+++ HW4/HW4.c
@@ -1,13 +1,38 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/i2c.h"
+#include "ssd1306.h"
+#include "font.h"
 
+#define I2C_PORT i2c0
+#define I2C_SDA 4
+#define I2C_SCL 5
+#define HEARTBEAT_LED PICO_DEFAULT_LED_PIN
+
+//void drawChar(uint16_t x, uint16_t y, char letter);
+//void drawMessage(uint16_t x, uint16_t y, char*m);
 
 int main()
 {
     stdio_init_all();
 
+    gpio_init(I2C_PORT, 1700*1000);
+
+    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
+    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
+    gpio_pull_up(I2C_SDA);
+    gpio_pull_up(I2C_SCL);
+
+    ssd1306_setup();
+    ssd1306_clear();
+    ssd1306_update();
+
     while (true) {
-        printf("Hello, world!\n");
+        ssd1306_drawPixel(10, 20, 1);
+        ssd1306_update();
+        sleep_ms(1000);
+        ssd1306_drawPixel(10, 20, 0);
+        ssd1306_update();
         sleep_ms(1000);
     }
 }

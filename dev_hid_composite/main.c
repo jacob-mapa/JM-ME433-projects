8039a10486d7090837dd152785f3296478fcad64
jacob-mapa <jacobmapa2027@u.northwestern.edu>
2026-05-13 14:06:51 -0500

    IMU helper functions and register definitions

--- dev_hid_composite/main.c
+++ dev_hid_composite/main.c
@@ -34,8 +34,14 @@
 
 #include "usb_descriptors.h"
 
+#define MPU_I2C_PORT i2c0
+#define MPU_I2C_SDA_PIN 4
+#define MPU_I2C_SCL_PIN 5
+
 #define MODE_BUTTON_PIN 15
 #define LED_PIN 19
+
+#define MPU_ADDR 0x68
 
 //--------------------------------------------------------------------+
 // MACRO CONSTANT TYPEDEF PROTYPES
